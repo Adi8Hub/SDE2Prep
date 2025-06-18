d73842635cc61256d49d8b51c8c3167764184a42
Adi8Hub <am_dev_stumped@outlook.com>
2025-06-18 18:23:53 +0530

    Added Brute and Optimal

--- SDE2Prep_Day1/Day01_DSA/TwoSum.cs
+++ SDE2Prep_Day1/Day01_DSA/TwoSum.cs
@@ -21,6 +21,26 @@ public class Solution
         }
         return new int[] { -1, -1 };
     }
+
+    // Brute
+    /*
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+        int n = nums.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int sum = nums[i] + nums[j];
+                if (sum == target)
+                    return [i, j];
+            }
+        }
+        return [];
+    }
+    */
 }
 
 ///*******Moved Main outside *********
