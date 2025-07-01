8cdc381c564399e72400646b5d23d9d4f0081c72
Adi8Hub <am_dev_stumped@outlook.com>
2025-07-01 23:04:18 +0530

    Added MaxWords & SumEqualsToSum

--- CodingTracker/Basics/HashingPart1/8_MaxWordsYouCanType/8_MaxWordsYouCanType.cs
+++ CodingTracker/Basics/HashingPart1/8_MaxWordsYouCanType/8_MaxWordsYouCanType.cs
@@ -1,11 +1,29 @@
-// 8_MaxWordsYouCanType
 using System;
 using System.Collections.Generic;
 
+Console.WriteLine(CanBeTypedWords("hello world", "ad")); // Output: 1
 class Solution
 {
-    static void Main()
+    public static int CanBeTypedWords(string text, string brokenLetters)
     {
-        Console.WriteLine("Run the solution logic here.");
+        HashSet<char> broken = new HashSet<char>(brokenLetters);
+        string[] words = text.Split(' ');
+        int count = 0;
+
+        foreach (var word in words)
+        {
+            bool canType = true;
+            foreach (char c in word)
+            {
+                if (broken.Contains(c))
+                {
+                    canType = false;
+                    break;
+                }
+            }
+            if (canType) count++;
+        }
+
+        return count;
     }
 }
--- CodingTracker/Basics/HashingPart1/9_SumEqualsToSum/9_SumEqualsToSum.cs
+++ CodingTracker/Basics/HashingPart1/9_SumEqualsToSum/9_SumEqualsToSum.cs
@@ -1,11 +1,37 @@
-// 9_SumEqualsToSum
 using System;
 using System.Collections.Generic;
 
+int[] A = { 3, 4, 7, 1, 2, 9, 8 };
+Console.WriteLine(Solution.HasEqualSumPairs(A)); // Output: 1
 class Solution
 {
-    static void Main()
+    public static bool HasEqualSumPairs(int[] A)
     {
-        Console.WriteLine("Run the solution logic here.");
+        int n = A.Length;
+        Dictionary<int, (int, int)> map = new Dictionary<int, (int, int)>();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int sum = A[i] + A[j];
+
+                if (map.ContainsKey(sum))
+                {
+                    var prev = map[sum];
+                    // Ensure all four indices are distinct
+                    if (prev.Item1 != i && prev.Item1 != j && prev.Item2 != i && prev.Item2 != j)
+                        return true;
+                }
+                else
+                {
+                    map[sum] = (i, j);
+                }
+            }
+        }
+
+        return false;
     }
+
+
 }
