53f158592ea885255d113cf4b1f320c60e0fb5ef
Aditya Mukherjee <63539030+Adi8Hub@users.noreply.github.com>
2025-07-01 21:46:57 +0530

    Delete CodingTracker/Strings directory

--- CodingTracker/Strings/2. CheckAnagram/Check.cs
+++
@@ -1,6 +0,0 @@
-/*
-Use map or freq count
-loop over chars and increase for s1 and decrease for s2
-
-Loop over the freq; if freq found !=0, then return false
-*/
--- CodingTracker/Strings/3. MaxWordsInSentence/MaxWords.cs
+++
@@ -1,9 +0,0 @@
-/*
-Loop over each sentence
-- Split it based on ' ' and count the length of split array
-OR
-- Now loop over each char of this sentence
--- if ' ' found, increase the word count
-
-if count > max, update max
-*/
