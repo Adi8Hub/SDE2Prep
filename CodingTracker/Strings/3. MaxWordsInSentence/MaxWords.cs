81e68e0b3f3da504bf0f324b99e9391031dea394
Adi8Hub <am_dev_stumped@outlook.com>
2025-07-01 13:14:47 +0530

    Solved All String Problem

---
+++ CodingTracker/Strings/3. MaxWordsInSentence/MaxWords.cs
@@ -0,0 +1,9 @@
+/*
+Loop over each sentence
+- Split it based on ' ' and count the length of split array
+OR
+- Now loop over each char of this sentence
+-- if ' ' found, increase the word count
+
+if count > max, update max
+*/
